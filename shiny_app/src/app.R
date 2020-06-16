295b7c2bf8e8080776ecb0dbb3eb27fd95402d3b
da-zar <dr.data@gmx.net>
2020-06-16 21:19:17 +0200

    change shiny app

--- shiny_app/src/app.R
+++ shiny_app/src/app.R
@@ -1,4 +1,47 @@
 library(shiny)
-ui <- fluidPage("Hi! I'm a shiny app!")
-server <- function(input, output){}
+ui <- fluidPage(
+  titlePanel("The normal distribution"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "mean",
+                  label = "Mean:",
+                  min = -10,
+                  max = 10,
+                  value = 0),
+      sliderInput(inputId = "sd",
+                  label = "Standard deviation:",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      sliderInput(inputId = "sample_size",
+                  label = "Sample size:",
+                  min = 1000,
+                  max = 100000,
+                  value = 10000),
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 100,
+                  value = 50)
+    ),
+    mainPanel(
+      plotOutput(outputId = "hist")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    x <- rnorm(input$sample_size, input$mean, input$sd)
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlim = c(-30, 30),
+         xlab = "X",
+         main = "Histogram")
+    
+  })
+  
+}
+
 shinyApp(ui = ui, server = server)
